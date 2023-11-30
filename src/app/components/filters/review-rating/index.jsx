48767979a4b8f9a2d3c69b5f39ee72b4961f2924
Luciano Germani <lucianogermani93@gmail.com>
2023-11-30 21:53:19 +0100

    Feat: Create step 2

--- src/app/components/filters/review-rating/index.jsx
+++ src/app/components/filters/review-rating/index.jsx
@@ -1,28 +1,54 @@
+import {  useEffect, useState } from 'react';
+import {  useDispatch }     from 'react-redux';
+import {updateReviewRating}                     from '@/app/redux/action'
+import { Rating } from 'react-simple-star-rating'
+import { IoIosArrowRoundForward } from "react-icons/io";
+import './index.scss';
 
 
-import { useSelector, useDispatch }     from 'react-redux';
-import {updateReviewRating}             from '@/app/redux/action'
+const ReviewRating = () => {
 
-const ReviewRating = ({handleInputChange,filterData}) => {
+   const dispatch = useDispatch();
+   const [ratingValue, setRatingValue] = useState(0);
+   const [continueButton,setContinueButton] = useState(false);
 
-    const dispatch = useDispatch();
-    const FilterData = useSelector((state) => state.FilterData);
 
-    /**
-     * @method
-     * Dispatch updateData Action
-     * @param {Int} year 
-     */
-    const handleUpdateReviewRating = () => {
-        const {reviewRating}        = filterData;
-        dispatch(updateReviewRating(reviewRating));
-        };
+   /**
+    * @method
+    * Set the ratingValue state and allow to next step
+    * 
+    * @param {int} rate Rate selected from Rating component
+    */
+   const handleRating = (rate) => {
+    setRatingValue(rate * 2);
+    setContinueButton(true)
+  }
+  
 
     return (
-        <div>
-            <label for="reviewRating">Por Arriba de que media de valoraciones quieres la pelicula?</label>
-            <input type='text' id='reviewRating' name='reviewRating'   onChange={handleInputChange}></input>
-            <button type="button" onClick={handleUpdateReviewRating}>click</button>
+        <div className='review-rating-design'>
+          <div className='step-two'>
+            <div className='description'>
+              <div className='step'>2.<IoIosArrowRoundForward /></div>
+              <div>
+                <h2 className='title'>Step 2: Rate the Excitement</h2>
+                <p className='subtitle'>Pick the Average Rating for Your Movie Experience</p>
+              </div>
+            </div>
+            <div className='star-rating-container'>
+                <Rating
+                transition
+                showTooltip
+                allowFraction
+                fillColorArray={['#f17a45', '#f19745', '#f1a545', '#f1b345', '#f1d045']} 
+                onClick={handleRating}
+                />
+            </div>
+            {
+              continueButton && 
+              <div className='next-step' onClick={() => dispatch(updateReviewRating(ratingValue))}>Step 3<IoIosArrowRoundForward/></div>
+            }
+          </div>
         </div>
     )
 }

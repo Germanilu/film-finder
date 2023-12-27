f59fc1bd7547db1d16d0394b97a8363c621d7c0f
Luciano Germani <lucianogermani93@gmail.com>
2023-12-27 20:26:01 +0100

    Feature: research movie

--- src/app/components/search/index.jsx
+++ src/app/components/search/index.jsx
@@ -1,10 +1,23 @@
-import { useSelector, useDispatch }             from 'react-redux';
+import { useSelector, useDispatch, useState }             from 'react-redux';
 import { get,includes,pull,map, lowerCase }     from 'lodash';
+import { IoIosArrowRoundForward, IoIosArrowRoundBack }  from "react-icons/io";
+import { previousStep}     from '@/app/redux/action'
+import './index.scss';
 
 const Search = () => {
 
-    const state = useSelector((state) => state);
-    const bearerToken = process.env.NEXT_PUBLIC_BEARER_TOKEN;
+  const dispatch = useDispatch();
+
+
+  const selectedYear =  useSelector((state) =>  get(state,'FilterData.year.year',null));
+  const selectedRating =  useSelector((state) =>  get(state,'FilterData.reviewRating',null));
+  const selectedactors =  useSelector((state) =>  get(state,'FilterData.actors',null));
+  const selectedcategory =  useSelector((state) =>  get(state,'FilterData.category',null));
+
+  const state = useSelector((state) => state);
+  const bearerToken = process.env.NEXT_PUBLIC_BEARER_TOKEN;
+
+  const [result, setResult] = useState([])
 
     /**
      * @method
@@ -13,17 +26,15 @@ const Search = () => {
      * @returns {Function} getMovie with processedData Object with all the filters informations
      */
     const prepareData = () => {
+      debugger
         //To join categories array into string
-        const {category} = get(state,'FilterData')
-        const joinValueInCategory = category.join('&')
+        const joinValueInCategory = selectedcategory.join('&')
         
         //To get correct year 
-        const {year} = get(state,'FilterData')
-        const date = `${year}-01-01`
+        const date = `${selectedYear}-01-01`
 
         //To join actors array into string
-        const {actors} = get(state,'FilterData')
-        const joinValueInActors = actors.join('|')
+        const joinValueInActors = selectedactors.join('|')
 
         //To get reviewRating
         const {reviewRating} = get(state,'FilterData')
@@ -38,6 +49,7 @@ const Search = () => {
     
     
     const getMovie = () =>  {
+
 
         const processedData = prepareData();
         //url base
@@ -66,14 +78,40 @@ const Search = () => {
         //fetch url
         fetch(url, options)
           .then(response => response.json())
+          .then(response => setResult(response))
           .then(response => console.log(response))
           .catch(err => console.error(err));
     }
 
-    
+    console.log("EE",response)
     return(
-        <div>
-            <button type="button" onClick={getMovie} >click</button>
+        <div className='search-container'>
+            <p>These are the filters you have selected</p>
+              <div className="selected-filters-box">
+                  <div className="box">
+                    <p>Year</p>
+                    <p>{selectedYear} +</p>
+                  </div>
+                  <div className="box">
+                    <p>Avg. Rating</p>
+                    <p>{selectedRating}</p>
+                  </div>
+                  <div className="box">
+                    <p>Category</p>
+                    <p>{selectedcategory}</p>
+                  </div>
+                  <div className="box">
+                    <p>Actors</p>
+                    <p>{selectedactors}</p>
+                  </div>
+
+              </div>
+              <div className="buttons">
+                <div className='previous-step' onClick={() => dispatch(previousStep())}><IoIosArrowRoundBack/>Step 4</div>
+                <div className='next-step' onClick={getMovie}>Search</div>
+              </div>
+
+              <div className="result-container"></div>
         </div>
     )
 }
